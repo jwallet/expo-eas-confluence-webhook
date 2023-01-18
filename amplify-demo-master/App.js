f75a5660ccb3b6907d295b4279e5ffeb2851dd37
Jwallet <wizik20@gmail.com>
2023-01-17 21:25:23 -0500

    app

---
+++ amplify-demo-master/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello from an Expo PWA served from AWS Amplify! Just push your changes to GitHub and Amplify will deploy them to production. Check out the <a href="https://github.com/expo/amplify-demo">GitHub repo</a>.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
